cb427ddbe7299d7ce0b06acc3f2cb20f59aa1798
Puillet <pierreguillet99@gmail.com>
2019-10-20 22:24:30 +0200

    Update JeauBean.java

--- src/model/JeuBean.java
+++ src/model/JeuBean.java
@@ -28,25 +28,14 @@ public class JeuBean implements Serializable {
     }
 
     public void verifReponse(HttpServletRequest request){
-        System.out.println("Check answer");
         reponseUser = Double.valueOf(request.getParameter(reponse));
-        System.out.println("User Answer : " + reponseUser);
         HttpSession session = request.getSession();
         Stack<Double> correctAnswer = (Stack<Double>) session.getAttribute("pile");
-        System.out.println("Correct Answer : " + correctAnswer);
-        //test pour valider le résultat de l'utilisateur, il peux rentrer une valeur arrondi à 0.0001 près
-        //ce test à du être mis en place ) cause de la conversion et de l'arrondi de l'ordinateur des valeurs
-        //et surtout à la longeur des résultat fournis
-        if (Math.abs(reponseUser - correctAnswer.pop()) < 0.01){
+        if (Math.abs(reponseUser - correctAnswer.pop()) < 0.01) {
             System.out.println("Résultat Correct");
-            //si le résultat est correct on incrémente de 1 le nombre de bonnes
-            session.setAttribute("nbVictoire", (int) session.getAttribute("nbVictoire") + 1);
-        }else {
-            System.out.println("Résultat incorrect");
+            session.setAttribute("nbCorrect", (int) session.getAttribute("nbCorrect") + 1);
         }
-        //dans tous les cas on incrémente de 1 le nombre de question traitée
-        session.setAttribute("nbQuestion", (int) session.getAttribute("nbQuestion") + 1);
-        System.out.println("Nombre total de question éffectué : " + session.getAttribute("nbQuestion"));
+        session.setAttribute("nbQuestions", (int) session.getAttribute("nbQuestions") + 1);
     }
 
     public void verifMeilleurScore(HttpServletRequest request){
